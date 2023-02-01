50116eb617bef683fb8a05ffab16dd980b248373
Ankit <ankitojha1409@gmail.com>
2023-02-02 04:26:24 +0530

    About me page customized

--- src/content_option.js
+++ src/content_option.js
@@ -1,132 +1,168 @@
 const logotext = "Ankit Ojha";
 const meta = {
-    title: "Ankit Ojha",
-    description: "I’m Ankit Ojha web developer_ Full stack devloper,currently studying CSE",
+  title: "Ankit Ojha",
+  description:
+    "I’m Ankit Ojha web developer_ Full stack devloper,currently studying CSE",
 };
 
 const introdata = {
-    title: "I’m Ankit Ojha",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
-    },
-    description: "Currently pursuing a Bachelor's Degree in Information Technology from Lovely Professional University (LPU) - Punjab, India.",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+  title: "I’m Ankit Ojha",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description:
+    "Currently pursuing a Bachelor's Degree in Information Technology from Lovely Professional University (LPU) - Punjab, India.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "about about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+  title: "About my self",
+  aboutme:
+    "I am a Computer Science and Engineering student. Having interest in Programming and web development. I used to create websites for fun. I enjoy doing coding and building something out of it.",
 };
-const worktimeline = [{
-        jobtitle: "Lead Graphic Designer",
-        where: "AWS Cloud Community LPU",
-        date: "2022",
-    },
-    {
-        jobtitle: "Head of Graphic Desig Team",
-        where: "GeeksforGeesk-LPU",
-        date: "2022",
-    },
-    
+const worktimeline = [
+  {
+    jobtitle: "Lead Graphic Designer",
+    where: "AWS Cloud Community LPU",
+    date: "2022",
+  },
+  {
+    jobtitle: "Head of Graphic Desig Team",
+    where: "GeeksforGeesk-LPU",
+    date: "2022",
+  },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 70,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
-    },
+const skills = [
+  {
+    name: "HTML",
+    value: 90,
+  },
+  {
+    name: "CSS",
+    value: 70,
+  },
+  {
+    name: "Figma",
+    value: 90,
+  },
+  {
+    name: "Tailwind CSS",
+    value: 80,
+  },
+
+  {
+    name: "JavaScript",
+    value: 70,
+  },
+
+  {
+    name: "React",
+    value: 60,
+  },
+
+  {
+    name: "Node JS",
+    value: 20,
+  },
+  {
+    name: "Python",
+    value: 60,
+  },
+  {
+    name: "Java",
+    value: 60,
+  },
+  {
+    name: "kotlin",
+    value: 30,
+  },
+  {
+    name: "Linux",
+    value: 20,
+  },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+const services = [
+  {
+    title: "Websites",
+    description:
+      "I will craete a fully Responsive and Intercative website for your business or Portfolio. I use React.JS/Next.JS to create frontend of the website. ",
+  },
+  {
+    title: "Android Apps",
+    description:
+      "Let's make an application for your next big idea. I will create android apps using Java/Kotlin.",
+  },
+  {
+    title: "UI & UX Design",
+    description:
+      "Ahh !! you have a business idea but you are unable to make a digital presence? Let's give a structure to your business idea. I use Figma to create UI screens.",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "GeeksforGeeks-LPU Official website.",
-        link: "https://geeksforgeekslpu.netlify.app/",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        desctiption: "UI Design. Gojo Ride App. Complete Ui on Figma.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        desctiption: "Simple portfolio website using HTML, CSS and JavaScript.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        desctiption: "Makup-cosmetic store web design in Figma.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        desctiption: "Demo agency website for a web developemnt agency.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        desctiption: "Home Page design for a Creative Studio website",
-        link: "#",
-    },
-    
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "GeeksforGeeks-LPU Official website.",
+    link: "https://geeksforgeekslpu.netlify.app/",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption: "UI Design. Gojo Ride App. Complete Ui on Figma.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "Simple portfolio website using HTML, CSS and JavaScript.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption: "Makup-cosmetic store web design in Figma.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption: "Demo agency website for a web developemnt agency.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption: "Home Page design for a Creative Studio website",
+    link: "#",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "ankitojha1409@gmail.com",
-    YOUR_FONE: "+91 8700091952",
-    description: "Want to collaborate on a project? Text me on whatsapp or connect on socials.",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "ankitojha1409@gmail.com",
+  YOUR_FONE: "+91 8700091952",
+  description:
+    "Want to collaborate on a project? Text me on whatsapp or connect on socials.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com/ankitojha07",
-    youtube: "https://youtube.com/@codeforcoffeee",
-    linkedin: "https://linkedin.com/in/ankitojha07",
-    twitter: "https://twitter.com/ankitojha_07",
+  github: "https://github.com/ankitojha07",
+  youtube: "https://youtube.com/@codeforcoffeee",
+  linkedin: "https://linkedin.com/in/ankitojha07",
+  twitter: "https://twitter.com/ankitojha_07",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
